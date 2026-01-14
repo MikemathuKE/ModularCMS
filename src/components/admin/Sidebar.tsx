99cf7ad6f42560ebce77b66426775b9f79b62236
MikemathuKE <michaelmathu6@gmail.com>
2026-01-14 15:03:27 +0300

    Collapsing Sidebar

--- src/components/admin/Sidebar.tsx
+++ src/components/admin/Sidebar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 import { pageMappings } from "@/utils/pageMappings";
+import * as Icons from "react-icons/fa";
 
 export const Sidebar: React.FC = () => {
   const router = useRouter();
@@ -11,6 +12,7 @@ export const Sidebar: React.FC = () => {
     email: "",
     role: "editor",
   });
+  const [fullSidebar, SetFullSidebar] = useState<boolean>(false);
 
   useEffect(() => {
     fetch("/api/auth/me")
@@ -26,29 +28,48 @@ export const Sidebar: React.FC = () => {
   };
 
   return (
-    <aside className="w-64 bg-white shadow-lg">
-      <div className="p-4 font-bold text-xl border-b">CMS Admin</div>
+    <aside
+      className="bg-white shadow-lg justify-center"
+      onMouseEnter={() => {
+        SetFullSidebar(true);
+      }}
+      onMouseLeave={() => {
+        SetFullSidebar(false);
+      }}
+    >
+      <div className="p-4 font-bold text-xl border-b w-full text-center">
+        Admin
+      </div>
       <nav className="p-4">
-        <ul className="space-y-2">
-          {pageMappings.map((item, index) => (
-            <>
-              {item.roles.includes(user.role) && (
-                <li key={index}>
-                  <Link href={item.path} key={item.name}>
-                    <span
-                      className={`block px-3 py-2 rounded-md cursor-pointer
-                      hover:bg-gray-100`}
-                      key={item.path}
+        <ul className="space-y-2 pb-4">
+          {pageMappings.map((item, index) => {
+            const IconComp = (Icons as any)[item.icon] || null;
+            return (
+              <>
+                {item.roles.includes(user.role) && (
+                  <li key={index}>
+                    <Link
+                      href={item.path}
+                      key={item.name}
+                      className="flex align-middle hover:bg-gray-100 cursor-pointer py-1 justify-center"
                     >
-                      {item.name}
-                    </span>
-                  </Link>
-                </li>
-              )}
-            </>
-          ))}
+                      {IconComp && <IconComp className="h-6 w-6" />}
+                      {fullSidebar && (
+                        <span
+                          className={`block px-3 rounded-md w-48`}
+                          key={item.path}
+                        >
+                          {item.name}
+                        </span>
+                      )}
+                    </Link>
+                  </li>
+                )}
+              </>
+            );
+          })}
         </ul>
-        <div className="mt-auto bottom-0 pt-4 border-t border-gray-700">
+        <div className="mt-auto bottom-0 pt-4 border-t border-gray-700 justify-center">
           <button
             onClick={handleLogout}
             className="bg-red-600 sticky bottom-0 text-white px-4 py-2 rounded hover:bg-red-700"
--- src/utils/pageMappings.ts
+++ src/utils/pageMappings.ts
@@ -2,51 +2,61 @@ export const pageMappings = [
   {
     name: "Dashboard",
     path: "/admin",
+    icon: "FaHome",
     roles: ["admin", "editor", "superuser"],
   },
   {
     name: "Layout",
     path: "/admin/layout",
+    icon: "FaObjectGroup",
     roles: ["admin", "editor", "superuser"],
   },
   {
     name: "Pages",
     path: "/admin/pages",
+    icon: "FaFile",
     roles: ["admin", "editor", "superuser"],
   },
   {
     name: "Media",
     path: "/admin/media",
+    icon: "FaCamera",
     roles: ["admin", "editor", "superuser"],
   },
   {
     name: "Content",
     path: "/admin/content",
+    icon: "FaDatabase",
     roles: ["admin", "editor", "superuser"],
   },
   {
     name: "Content Types",
     path: "/admin/contenttypes",
+    icon: "FaQuestionCircle",
     roles: ["admin", "editor", "superuser"],
   },
   {
     name: "Themes",
     path: "/admin/themes",
+    icon: "FaPaintBrush",
     roles: ["admin", "editor", "superuser"],
   },
   {
     name: "Users",
     path: "/admin/users",
+    icon: "FaUsers",
     roles: ["admin", "superuser"],
   },
   {
     name: "Tenants",
     path: "/admin/tenants",
+    icon: "FaServer",
     roles: ["admin", "superuser"],
   },
   {
     name: "Settings",
     path: "/admin/settings",
+    icon: "FaCog",
     roles: ["admin", "editor", "superuser"],
   },
 ];
